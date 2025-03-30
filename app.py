9f831c57d3f7cdebd654014f75e7cd8aa876ba44
Mactyws <mattiagirellini165@gmail.com>
2025-03-30 16:38:47 +0200

    fixed static files not loading

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from scrapers import flights, hotels, attractions
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def home():
+def index():
     # load index.html
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
+@app.route('/chatbot')
+def chatbot():
+    # load index.html
+    return render_template('chatbot.html')
+
+@app.route('/faq')
+def faq():
+    # load index.html
+    return render_template('faq.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
